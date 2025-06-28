40744de76bf6362799d86d8a1c637b52f06b2e0d
andubandu <lolandu575@gmail.com>
2025-06-28 22:52:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 app.use(session({
   secret: process.env.JWT_SECRET,
   resave: false,
