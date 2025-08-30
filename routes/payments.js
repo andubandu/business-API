dbbdaaecdb6df3b9eb7960232f039ffd9105f447
andubandu <lolandu575@gmail.com>
2025-08-30 16:24:42 +0400

    reuploaded modded code

--- routes/payments.js
+++ routes/payments.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { authMiddleware } = require('../middleware/auth');
 const { validate, validateParams, schemas } = require('../middleware/validation');
